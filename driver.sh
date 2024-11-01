5097c6765bebb584084d777b9330a1e0d0cbff97
anhacvai11 <90373481+anhacvai11@users.noreply.github.com>
2024-11-01 13:13:32 +0700

    Update driver.sh

--- driver.sh
+++ driver.sh
@@ -4,7 +4,7 @@ sudo ubuntu-drivers install
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb
 sudo apt install -y ./cuda-keyring_1.1-1_all.deb
 sudo apt update
-sudo apt -y install cuda-toolkit-12-5
+sudo apt -y install cuda-toolkit-11-8
 sudo apt update
 sudo apt -y full-upgrade 
 
