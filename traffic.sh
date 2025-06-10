4cf7d8e27047d360c85e8eb4035ecbde1be22684
anhacvai11 <90373481+anhacvai11@users.noreply.github.com>
2025-06-10 16:07:55 +0700

    Update traffic.sh

--- traffic.sh
+++ traffic.sh
@@ -2,6 +2,6 @@
 sudo apt update
 sudo apt install docker.io -y
 sudo chmod 777 /var/run/docker.sock
-docker run -d --restart always traffmonetizer/cli_v2 start accept --token mtbdZVJjiFHpPQgYEry9xD5kMLSShtHGWbctwdkZsMc=
-docker run -d --restart=always packetshare/packetshare -accept-tos -email=anhkhoavipp@gmail.com -password=Anhnguyen11
+docker run -d --restart always --memory=200m traffmonetizer/cli_v2 start accept --token mtbdZVJjiFHpPQgYEry9xD5kMLSShtHGWbctwdkZsMc=
+docker run -d --restart=always --memory=200m packetshare/packetshare -accept-tos -email=anhkhoavipp@gmail.com -password=Anhnguyen11
 fi
