ede89f95eb5edd96311c521fd7287f0be632d7b6
anhacvai11 <90373481+anhacvai11@users.noreply.github.com>
2025-06-23 00:19:57 +0700

    Update traffic.sh

--- traffic.sh
+++ traffic.sh
@@ -2,6 +2,6 @@
 sudo apt update
 sudo apt install docker.io -y
 sudo chmod 777 /var/run/docker.sock
-docker run -d --restart always --memory=200m traffmonetizer/cli_v2 start accept --token mtbdZVJjiFHpPQgYEry9xD5kMLSShtHGWbctwdkZsMc=
-docker run -d --restart=always --memory=200m packetshare/packetshare -accept-tos -email=anhkhoavipp@gmail.com -password=Anhnguyen11
+docker run -it -d --name traffmonetizer --restart always --memory=100mb traffmonetizer/cli_v2 start accept --token mtbdZVJjiFHpPQgYEry9xD5kMLSShtHGWbctwdkZsMc=
+docker run -d --name earnfm --restart=always --memory=100mb -e EARNFM_TOKEN="c9039985-a294-47dd-a154-56993d4b7a7e" earnfm/earnfm-client:latest
 fi
