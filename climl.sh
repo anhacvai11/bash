689116ef56f0872affe7fa7fa6b4284d688ef381
anhacvai11 <90373481+anhacvai11@users.noreply.github.com>
2025-07-22 11:59:14 +0700

    Create climl.sh

---
+++ climl.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt install -y python3 python3-pip python3-venv
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+
+pip3 install azureml-sdk
+pip3 install azure-cli
+pip install paramiko
